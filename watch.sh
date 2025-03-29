ad10a7e3985a4a6dba6e1f87060e8f29b96877aa
zomb <zomb@ie>
2025-03-29 03:44:00 -0400

    make progress scripts more robust to reboots

--- watch.sh
+++ watch.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-watch -t -n 1 "./progress.sh && df -h /dev/sdb1 && ls -hlt *.log | head -n 27 && echo ... && ls -hlt *.log | tail -n 1"
+watch -t -n 1 "./progress.sh && df -h /mnt/small && ls -hlt *.log | head -n 27 && echo ... && ls -hlt *.log | tail -n 1"
