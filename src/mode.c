a2fe4c21c18a29e12563bfc8c350b6d76727ba4b
RusDoomer <aklrusdoomer@gmail.com>
2025-05-31 04:08:37 -0400

    algorithm 6.1 simulated annealing metropolis probability function, 1 swap at a time, 10 starting temperature, exponential cooling function, 5.0 alpha, minimum temperature 0.01, random swaps

--- src/mode.c
+++ src/mode.c
@@ -288,9 +288,9 @@ void *thread_function(void *arg) {
      * future: 2, 4, *slowly decreasing
      *
      * initial temperature (T):
-     * past: 100, 10
-     * current: 1000
-     * future:
+     * past:
+     * current: 10
+     * future: 1000, 100
      *
      * cooling function:
      * past: linear
@@ -298,14 +298,14 @@ void *thread_function(void *arg) {
      * future: *logarithmic
      *
      * cooling rate (alpha):
-     * past: 5.0, 8.0
-     * current: 3.0
+     * past: 3.0, 8.0
+     * current: 5.0
      * future:
      *
      * minimum temperature (min_T):
-     * past:
-     * current: 1.0
-     * future: 0.01, *100
+     * past: 1.0
+     * current: 0.01
+     * future: *100
      *
      * how neighbors are chosen:
      * past:
@@ -362,13 +362,13 @@ void *thread_function(void *arg) {
     clock_gettime(CLOCK_MONOTONIC, &start);
 
     /* Initial temperature */
-    float T = 1000.0;
+    float T = 10.0;
     /* Max temp */
     const float max_T = T;
     /* Min temp */
-    const float min_T = 1.0;
+    const float min_T = 0.01;
     /* Cooling rate */
-    const float alpha = 3.0;
+    const float alpha = 5.0;
     /* Number of swaps */
     int swap_count = 1;
 
@@ -536,9 +536,9 @@ void improve(int shuffle) {
     // Step 2: Combine with weight_name into the filename
     char filename[256];
     if (shuffle) {             //prob function, swaps + type of swaps, cooling func,        temp start-end
-        snprintf(filename, sizeof(filename), "simulated_annealing_M_1R_E3_1000-1_log_%s_shuffle_%d_%d_%s.log", weight_name, threads, repetitions, timestamp);
-    } else {                   //metroid,       1 random swap,         exponential alpha=3, T=1000 min_T=1
-        snprintf(filename, sizeof(filename), "simulated_annealing_M_1R_E3_1000-1_log_%s_%s_%d_%d_%s.log", weight_name, layout_name, threads, repetitions, timestamp);
+        snprintf(filename, sizeof(filename), "simulated_annealing_M_1R_E5_10-001_log_%s_shuffle_%d_%d_%s.log", weight_name, threads, repetitions, timestamp);
+    } else {                   //metroid,       1 random swap,         exponential alpha=5, T=10 min_T=0.01
+        snprintf(filename, sizeof(filename), "simulated_annealing_M_1R_E5_10-001_log_%s_%s_%d_%d_%s.log", weight_name, layout_name, threads, repetitions, timestamp);
     }
     // Open log file in append mode with UTF-8 encoding
     FILE *logfile = fopen(filename, "a"); // Text mode for UTF-8 compatibility
